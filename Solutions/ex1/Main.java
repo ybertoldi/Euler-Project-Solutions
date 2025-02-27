bb422dd85012781d58e00a4baa7cfe6a22b4fca2
yuri <yuri.bertoldi003@gmail.com>
2025-02-27 09:18:13 -0300

    second commit

---
+++ Solutions/ex1/Main.java
@@ -0,0 +1,7 @@
+package Solutions.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Solutions/ex2/Main.java
@@ -0,0 +1,7 @@
+package Solutions.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
